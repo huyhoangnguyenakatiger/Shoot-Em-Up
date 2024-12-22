5ca7c91a877fed5577845563ad83e724e0023dea
huyhoangnguyenakatiger <huyhoangnguyenakatiger@gmail.com>
2024-12-22 23:45:06 +0700

    first commit

---
+++ Assets/_Project/Scripts/InputReader.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace shmup
+{
+    // [RequireComponent(typeof(PlayerInput))]
+    public class InputReader : MonoBehaviour
+    {
+        PlayerInput playerInput;
+        InputAction moveAction;
+
+        void Awake()
+        {
+            playerInput = GetComponent<PlayerInput>();
+            moveAction = playerInput.actions["Move"];
+        }
+
+        public Vector2 Move => moveAction.ReadValue<Vector2>();
+    }
+
+
+}
---
+++ Assets/_Project/Scripts/PlayerController.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+
+namespace shmup
+{
+    public class PlayerController : MonoBehaviour
+    {
+
+    }
+}
